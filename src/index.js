092f1754f87c1aa5078e61b6c1a3da75d6155519
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2022-10-02 22:06:13 -0300

    js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+let stopwatch = document.querySelector("#stopwatch");
+let btnStart = document.querySelector("#start-button");
+
+let seconds = 0;
+
+btnStart.addEventListener("click", start);
+
+function start() {}
+
+function stop() {}
+function clear() {}
+function addOne() {}
+
+function minusOne() {}
+
+function setLap() {}
